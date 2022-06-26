b45c1409d86b6e395c5929c3092919f04a8b44f5
Milan Rajani <milanrajani33@gmail.com>
2022-06-26 15:23:48 +0530

    routes bug fixed

--- src/App.js
+++ src/App.js
@@ -31,13 +31,15 @@ export default class App extends Component {
              
           />
           <Routes>
-            <Route exact path="/" element={<News setProgress={this.setProgress} key="General" pageSize={5} country="in" category="General" />} />
-            <Route exact path="/Entertainment" element={<News setProgress={this.setProgress} key="Entertainment" pageSize={5} country="in" category="Entertainment" />} />
-            <Route exact path="/Business" element={<News setProgress={this.setProgress} key="Business" pageSize={5} country="in" category="Business" />} />
-            <Route exact path="/Health" element={<News setProgress={this.setProgress} key="Health" pageSize={5} country="in" category="Health" />} />
-            <Route exact path="/Science" element={<News setProgress={this.setProgress} key="Science" pageSize={5} country="in" category="Science" />} />
-            <Route exact path="/Sports" element={<News setProgress={this.setProgress} key="Sports" pageSize={5} country="in" category="Sports" />} />
-            <Route exact path="/Technology" element={<News setProgress={this.setProgress} key="Technology" pageSize={5} country="in" category="Technology" />} />
+            <Route exact path="/" element={<News setProgress={this.setProgress} key="General" pageSize={100} country="in" category="general" />} />
+            <Route exact path="/Entertainment" element={<News setProgress={this.setProgress} key="Entertainment" pageSize={100} country="in" category="Entertainment" />} />
+            <Route exact path="/Business" element={<News setProgress={this.setProgress} key="Business" pageSize={100} country="in" category="Business" />} />
+            <Route exact path="/Health" element={<News setProgress={this.setProgress} key="Health" pageSize={100} country="in" category="Health" />} />
+            <Route exact path="/general" element={<News setProgress={this.setProgress} key="general" pageSize={100} country="in" category="General" />} />
+            <Route exact path="/Science" element={<News setProgress={this.setProgress} key="Science" pageSize={100} country="in" category="Science" />} />
+            <Route exact path="/Sports" element={<News setProgress={this.setProgress} key="Sports" pageSize={100} country="in" category="Sports" />} />
+            <Route exact path="/Technology" element={<News setProgress={this.setProgress} key="Technology" pageSize={100} country="in" category="Technology" />} />
+            <Route exact path="/NewsApp" element={<News setProgress={this.setProgress} key="Technology" pageSize={100} country="in" category="Technology" />} />
 
           </Routes>
         </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,22 +5,23 @@ import { Link } from 'react-router-dom';
 
 
 export default function Navbar(props) {
-  return (
+  return ( 
     <nav className={`navbar fixed-top navbar-expand-lg navbar navbar-dark bg-dark `}>
       <div className="container-fluid">
-        <Link className="navbar-brand disabled" to="/">Designed by Milan Rajani</Link>
+        <Link className="navbar-brand aria-disabled" to="/">Designed by Milan Rajani</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item"><Link to="Business" className="nav-link " >Business</Link></li>
-            <li className="nav-item"><Link to="Entertainment" className="nav-link " >Entertainment</Link></li>
-            <li className="nav-item"><Link to="/" className="nav-link " >General</Link></li>
-            <li className="nav-item"><Link to="Health" className="nav-link " >Health</Link></li>
-            <li className="nav-item"><Link to="Science" className="nav-link " >Science</Link></li>
-            <li className="nav-item"><Link to="Sports" className="nav-link " >Sports</Link></li>
-            <li className="nav-item"><Link to="Technology" className="nav-link " >Technology</Link></li>
+          {/* <Link className="nav-link" aria-current="page" to="/">Home</Link> */}
+            <li className="nav-item"><Link to="/business" className="nav-link " >Business</Link></li>
+            <li className="nav-item"><Link to="/Entertainment" className="nav-link " >Entertainment</Link></li>
+            <li className="nav-item"><Link to="/General" className="nav-link " >General</Link></li>
+            <li className="nav-item"><Link to="/Health" className="nav-link " >Health</Link></li>
+            <li className="nav-item"><Link to="/Science" className="nav-link " >Science</Link></li>
+            <li className="nav-item"><Link to="/Sports" className="nav-link " >Sports</Link></li>
+            <li className="nav-item"><Link to="/Technology" className="nav-link " >Technology</Link></li>
           </ul>
           
         </div>
